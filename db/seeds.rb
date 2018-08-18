6dc8fe1ae6a522aa7dd903f19de656db4a572b62
RajaaBoulassouak <raj.b.stretz@gmail.com>
2018-08-17 20:06:28 -0600

    adds invoice seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@ CSV.foreach('./data/merchants.csv', headers: true, header_converters: :symbol) d
                   created_at: merchant[:created_at],
                   updated_at: merchant[:updated_at])
 end
+
+CSV.foreach('./data/invoices.csv', headers: true, header_converters: :symbol) do |invoice|
+  Invoice.create(id: invoice[:id],
+                 customer_id: invoice[:customer_id],
+                 merchant_id: invoice[:merchant_id],
+                 status: invoice[:status],     
+                 created_at: invoice[:created_at],
+                 updated_at: invoice[:updated_at])
+end
