ff2b6d9c0705deb47edfb5edd0db43b61c61c012
Patrick Shobe <patrick@patrickshobe.com>
2018-08-19 13:46:03 -0600

    quick bug fix

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,24 +1,24 @@
 RSpec.describe Item do
   describe 'validations' do
-    it 'is invalid without a title' do
+    it 'is invalid without a name' do
     item = Item.create( id:          1,
                         description: "i'm an item!",
-                        price:       200,
+                        unit_price:       200,
                         image:       'path/to/image.jpg')
 
     expect(item).to_not be_valid
     end
     it 'is invalid without a description' do
       item = Item.create( id:    2,
-                          title: 'Pineapple',
-                          price: 200,
+                          name: 'Pineapple',
+                          unit_price: 200,
                           image: 'path/to/image.jpg')
 
       expect(item).to_not be_valid
     end
     it 'is invalid without a price' do
       item = Item.create( id:          3,
-                          title:       'Thing',
+                          name:       'Thing',
                           description: 'definitely still a thing',
                           image:       '/path/to/image.jpg')
 
@@ -26,9 +26,9 @@ RSpec.describe Item do
     end
     it 'is invalid without an image' do
       item = Item.create(id: 4,
-                        title: 'Another thing',
+                        name: 'Another thing',
                          description: 'and another thing',
-                         price: 200)
+                         unit_price: 200)
       expect(item).to_not be_valid
     end
   end
